c809df6cf3b80b3179a6cc6501f08e93b7bbaaa0
Alex <al.repin.a@yandex.ru>
2022-09-09 11:55:53 +0300

    add Task041

---
+++ Task041/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь. Ввод чисел происходит
+// через Enter, останавливается при введении слова "stop"
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+void PositiveNumber()
+{
+    Console.WriteLine("Введите число");
+    string number = Console.ReadLine();
+    int count = 0;
+    if (number == "stop")
+    {
+        Console.WriteLine(count);
+    }
+    else
+    {
+        int num = Convert.ToInt32(number);
+        if (num > 0) count++;
+    }
+    Console.WriteLine(count);
+}
+PositiveNumber();
